a80ef8c04a41e074333a6f3f6b5018c78220d5ad
laura05010 <lcmmadrid5@gmail.com>
2022-08-26 21:45:19 -0400

    adding projects from course into github

---
+++ I Am Healthy/I Am Healthy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Healthy
+//
+//  Created by Laura M Madrid on 2021-05-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ arrayDeepDive.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let numbers = [45, 73, 195, 53]
+
+var n1 = numbers[0]
+var n2 = numbers[1]
+var n3 = numbers[2]
+var n4 = numbers[3]
+
+let computedNumbers = [n1*n2,n2*n3, n3*n4, n4*n1]
+print(computedNumbers)
